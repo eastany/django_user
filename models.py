27dde1a7f5f6451ebaf4ea78f522a9857ada76ba
Nick <eastany@gmail.com>
2013-11-08 20:46:25 +0800

    asd
    asd

---
+++ models.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+class Page(models.Model):
+  name = models.CharField(max_length=64)
+  desc = models.CharField(max_length=64)
+  
+class Category(models.Model):
+  types = models.IntegerField()  #读写等
+  
+
+class ownPermission(models.Model):
+  page = models.ForeignKey(Page)
+  perm = models.ForeignKey(Category)
+  
+  
