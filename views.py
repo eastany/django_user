257b7bc4f02f11774ebafe649b33132fd75b51b4
Nick <eastany@gmail.com>
2013-11-08 20:59:49 +0800

    asd
    asd

---
+++ views.py
@@ -0,0 +1,20 @@
+
+def getPerm(user):
+	perms_list = []
+	perms = Permission.objects.filter(user=User.objects.get(username=user))
+	for perm in perms:
+		perms_list.append({perm.page.name: perm.category.types})
+		
+	return perms_list
+	
+def addUser(user,pwd,perms):
+	usr = User(username=user,password=pwd,is_staf=True)
+	usr.save()
+	
+	for key in perms.keys():
+		page = Page.objects.get(name=key)
+		cate = Category.objects.get(types=perms[key])
+		perm = Permission(user=usr,page=page,category=cate)
+		perm.save()
+		
+		
